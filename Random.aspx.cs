c6ecd922c97a80d384eda0728a4482856559ed57
Abram John Limpin <abramjohn.limpin@benilde.edu.ph>
2014-07-27 12:47:35 +0800

    Allow Display of Random Post

---
+++ Random.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Random : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(Helper.GetCon());
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        RandomPost();
+    }
+
+    void RandomPost()
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = "SELECT TOP 1 PostID FROM Posts ORDER BY NEWID()";
+        int postID = (int)cmd.ExecuteScalar();
+        con.Close();
+        Response.Redirect("Details.aspx?suggestion=" + postID.ToString());
+    }
+}
